a910de8346bdd7521d9e1741d6e2cb887121529a
winstonsmith1897 <m.simoni5@studenti.unipi.it>
2023-07-19 10:44:33 +0200

    test_transform_json_to_instance

--- test_kernel_analytic.py
+++ test_kernel_analytic.py
@@ -3,6 +3,7 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 import catch_topic
+import kernel_classification
 
 
 @pytest.fixture
@@ -17,6 +18,56 @@ def mock_kernel_classification():
         "catch_topic.kernel_classification.receive_data"
     ) as mock_kernel_classification:
         yield mock_kernel_classification
+
+
+def test_transform_json_to_instance():
+    # Dato un dato JSON con solo alcuni attributi validi
+    json_data = '{"brk": 1, "openat": 2, "read": 3}'
+    expected_instance = [
+        1,
+        0,
+        0,
+        0,
+        2,
+        0,
+        0,
+        3,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+    ]
+
+    # Chiama la funzione transform_json_to_instance con il dato JSON
+    instance = kernel_classification.transform_json_to_instance(json_data)
+    print("expected instance: " + str(expected_instance))
+    print("instance: " + str(instance))
+
+    # Verifica che l'istanza restituita sia uguale a quella attesa
+    assert instance == expected_instance
 
 
 def test_on_message(mock_kernel_classification):
